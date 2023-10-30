a41a78ca28b720a3f862233e46eb7bc63c03cb1d
Bimmerlynge <mathiashome@hotmail.com>
2023-10-30 16:49:29 +0100

    test(1): ZOMBIES tests for GetActuatorDetails

--- EndToEndTests/GetActuatorTests.cs
+++ EndToEndTests/GetActuatorTests.cs
@@ -25,13 +25,89 @@ public class GetActuatorTests : PageTest
     }
 
     [Test]
-    public async Task Test1()
+    public async Task GetActuatorDetails_ShouldNotifyError_WhenWONumberIsEmpty()
     {
-        await Page.Locator("input[name=\"woNo\"]").FillAsync("555555");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await Expect(Page.GetByText("WO Number must be 8 digits")).ToBeVisibleAsync();
+    }
+
+    [Test]
+    public async Task GetActuatorDetails_ShouldNotifyError_WhenWONumberIsLargerThan8Digits()
+    {
+        await Page.Locator("input[name=\"woNo\"]").FillAsync("123456789");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await Expect(Page.GetByText("WO Number must be 8 digits")).ToBeVisibleAsync();
     }
     
     [Test]
-    public async Task Test2()
+    public async Task GetActuatorDetails_ShouldNotifyError_WhenWONumberIsLessThan8Digits()
+    {
+        await Page.Locator("input[name=\"woNo\"]").FillAsync("1234567");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await Expect(Page.GetByText("WO Number must be 8 digits")).ToBeVisibleAsync();
+    }
+    
+    [Test]
+    public async Task GetActuatorDetails_ShouldAcceptInput_WhenWONumberIs8Digits()
+    {
+        await Page.Locator("input[name=\"woNo\"]").FillAsync("12345678");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await Expect(Page.GetByText("WO Number must be 8 digits")).ToBeHiddenAsync();
+    }
+
+    [Test]
+    public async Task GetActuatorDetails_ShouldNotifyError_WhenSerialNumberIsEmpty()
+    {
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await Expect(Page.GetByText("Serial Number must be 1-4 digits")).ToBeVisibleAsync();
+    }
+    
+    [Test]
+    public async Task GetActuatorDetails_ShouldNotifyError_WhenSerialNumberIsLargerThan4Digits()
+    {
+        await Page.Locator("input[name=\"serialNo\"]").FillAsync("12345");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await Expect(Page.GetByText("Serial Number must be 1-4 digits")).ToBeVisibleAsync();
+    }
+    
+    [Test]
+    public async Task GetActuatorDetails_ShouldNotifyError_WhenSerialNumberIsLessThan4Digits()
+    {
+        await Page.Locator("input[name=\"serialNo\"]").FillAsync("123");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await Expect(Page.GetByText("Serial Number must be 1-4 digits")).ToBeHiddenAsync();
+    }
+    
+    [Test]
+    public async Task GetActuatorDetails_ShouldNotifyError_WhenSerialNumberIs4Digits()
+    {
+        await Page.Locator("input[name=\"serialNo\"]").FillAsync("1234");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await Expect(Page.GetByText("Serial Number must be 1-4 digits")).ToBeHiddenAsync();
+    }
+    
+    [Test]
+    public async Task GetActuatorDetails_ShouldAcceptInput_WhenSerialNumberIs1Digit()
+    {
+        await Page.Locator("input[name=\"woNo\"]").FillAsync("1");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await Expect(Page.GetByText("Serial Number must be 1-4 digits")).ToBeHiddenAsync();
+    }
+    
+
+    [Test]
+    public async Task GetActuatorInfo_ShouldNotifyError_WhenNotFound()
+    {
+        await Page.Locator("input[name=\"woNo\"]").FillAsync("12345678");
+        await Page.Locator("input[name=\"serialNo\"]").FillAsync("1");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        var container = Page.Locator("#alert-container");
+        await Expect(container).ToBeVisibleAsync();
+        await Expect(container.Filter(new() {HasText = "Could not find actuator"})).ToBeVisibleAsync();
+    }
+    
+    [Test]
+    public async Task GetActuatorInfo_ShouldReturnPCBAUid_WhenFound()
     {
         await Page.Locator("input[name=\"woNo\"]").FillAsync("30912893");
         await Page.Locator("input[name=\"serialNo\"]").FillAsync("1");
