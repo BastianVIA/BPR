95e17f62321cd3341340b5c011afb883a9966eaf
Bimmerlynge <mathiashome@hotmail.com>
2023-11-27 11:21:43 +0100

    test(1): GetConfigurationControllerTests.cs

--- Backend.Tests/Controllers/GetConfigurationControllerTests.cs
+++ Backend.Tests/Controllers/GetConfigurationControllerTests.cs
@@ -1,39 +1,31 @@
 ﻿using AutoFixture;
 using AutoFixture.AutoNSubstitute;
 using Backend.Controllers;
+using Backend.Tests.Util;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Moq;
-using NSubstitute;
 
 namespace Backend.Tests.Controllers;
 
 public class GetConfigurationControllerTests
 {
     private Fixture _fixture = new();
-    private MockConfiguration _configuration = new();
-    
+
+    private IConfiguration _configuration = GetConfig.GetTestConfig();
     private GetConfigurationController _controller;
 
     public GetConfigurationControllerTests()
     {
-        _fixture.Customize(new AutoNSubstituteCustomization());
-        //_configuration = _fixture.Freeze<IConfiguration>();
         _controller = new GetConfigurationController(_configuration);
     }
 
     [Fact]
     public async Task GetAsync_ReturnsConfigurationResponse_WhenCalled()
     {
-        var expectedWoNoLength = _fixture.Create<int>();
-        var expectedSerialNumberMinLength = _fixture.Create<int>();
-        var expectedSerialNumberMaxLength = _fixture.Create<int>();
-
-        
-        
-
-        
-        var result = _controller.GetAsync(CancellationToken.None);
-        Console.WriteLine();
-        
+        var result = _controller.GetAsync(CancellationToken.None) as ObjectResult;
+        var response = result.Value;
+        Assert.NotNull(result);
+        Assert.IsType<ConfigurationResponse>(response);
+        Assert.IsType<OkObjectResult>(result);
     }
 }
