e1bb015c84c4f96ee56b167910259642519f1a19
Bimmerlynge <mathiashome@hotmail.com>
2023-11-29 09:26:59 +0100

    feat(3): Setting up page + code behind

--- Frontend/Pages/ActuatorInfo.razor.cs
+++ Frontend/Pages/ActuatorInfo.razor.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Frontend.Entities;
+using Microsoft.AspNetCore.Components;
 
 namespace Frontend.Pages;
 
 public class ActuatorInfoBase : ComponentBase
 {
-    
+
+    public List<Actuator> _actuators = new()
+    {
+        new Actuator() { SerialNumber = 123, WorkOrderNumber = 6575, PCBA = { PCBAUid = 12345}},
+        new Actuator() { SerialNumber = 123, WorkOrderNumber = 6575, PCBA = { PCBAUid = 12345}},
+        new Actuator() { SerialNumber = 123, WorkOrderNumber = 6575, PCBA = { PCBAUid = 12345}},
+    };
 }
