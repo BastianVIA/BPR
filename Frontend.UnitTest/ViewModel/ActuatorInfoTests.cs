48e17ce2a079fecf983363a06b46ff4d501cdc8b
Bimmerlynge <mathiashome@hotmail.com>
2023-11-29 10:23:11 +0100

    test(3): Setting up unit test for viewModel

---
+++ Frontend.UnitTest/ViewModel/ActuatorInfoTests.cs
@@ -0,0 +1,30 @@
+﻿using Frontend.Entities;
+using Frontend.Model;
+using Frontend.Pages;
+
+namespace Frontend.UnitTest.ViewModel;
+
+public class ActuatorInfoTests
+{
+    private Fixture _fixture = new();
+    private IActuatorDetailsModel _model = Substitute.For<IActuatorDetailsModel>();
+    private ActuatorInfoBase _viewModel;
+
+    public ActuatorInfoTests()
+    {
+        _viewModel = new ActuatorInfoBase(_model);
+    }
+
+    [Fact]
+    public async Task SearchPcba_ShouldReturnEmptyList_WhenNoMatchesFound()
+    {
+        var expected = new List<Actuator>();
+
+        _model.GetActuatorsByUid(Arg.Any<string>())
+            .Returns(expected);
+        
+        await _viewModel.SearchPcba();
+        
+        Assert.True(_viewModel.actuators.Count == 0);
+    }
+}
