06abad958eb09e6024d1b954b580e340981970be
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-12-01 20:17:17 +0100

    fix(2): clarification

--- BuildingBlocks/BuildingBlocks/Application/ICommand.cs
+++ BuildingBlocks/BuildingBlocks/Application/ICommand.cs
@@ -2,7 +2,7 @@
 
 namespace BuildingBlocks.Application;
 
-// All commands should have "private set" properties, in case they are used in inbox pattern where private set is needed
+// All commands should have public properties with "private set", in case they are used in inbox pattern where private set is needed
 public interface ICommand:IRequest
 {
 }
