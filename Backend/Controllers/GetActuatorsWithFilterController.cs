c0a753cd30863c75ac7dfcb12855107c513ec525
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-12-03 11:45:40 +0100

    feat(2): added woNo og serialNo til filter

--- Backend/Controllers/GetActuatorsWithFilterController.cs
+++ Backend/Controllers/GetActuatorsWithFilterController.cs
@@ -19,11 +19,11 @@ public class GetActuatorsWithFilterController : ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetActuatorWithFilterResponse))]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
     [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
-    public async Task<IActionResult> GetAsync([FromQuery] int woNo, [FromQuery] int serialNo, [FromQuery] string? pcbaUid, [FromQuery] string? itemNo,
+    public async Task<IActionResult> GetAsync([FromQuery] int? woNo, [FromQuery] int? serialNo, [FromQuery] string? pcbaUid, [FromQuery] string? itemNo,
         [FromQuery] int? manufacturerNo,
         [FromQuery] int? productionDateCode, CancellationToken cancellationToken)
     {
-        var query = GetActuatorsWithFilterQuery.Create(pcbaUid, itemNo, manufacturerNo, productionDateCode);
+        var query = GetActuatorsWithFilterQuery.Create(woNo, serialNo, pcbaUid, itemNo, manufacturerNo, productionDateCode);
         var result = await _bus.Send(query, cancellationToken);
         return Ok(GetActuatorWithFilterResponse.From(result));
     }
