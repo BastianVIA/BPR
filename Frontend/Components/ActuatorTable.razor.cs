cb04c8adfbfa43412f891ee9de82288ed3dd762a
Bimmerlynge <mathiashome@hotmail.com>
2023-12-03 13:52:49 +0100

    feat(4): Code-behind

---
+++ Frontend/Components/ActuatorTable.razor.cs
@@ -0,0 +1,18 @@
+﻿using Frontend.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Frontend.Components;
+
+public class ActuatorTableBase : ComponentBase
+{
+    [Parameter]
+    public List<Actuator> Actuators { get; set; } = new();
+    
+    public List<string> Filters { get; set; } = new();
+
+    public void UpdateFilters(List<string>? filters)
+    {
+        filters ??= new List<string>();
+        Filters = filters;
+    }
+}
