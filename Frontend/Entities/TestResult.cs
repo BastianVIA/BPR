d30dc2d1a3293560d73fef8d056f9903c197dcbd
Bimmerlynge <mathiashome@hotmail.com>
2023-12-05 13:45:21 +0100

    Creating page for TestResults

---
+++ Frontend/Entities/TestResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Frontend.Entities;
+
+public class TestResult
+{
+    public int WorkOrderNumber { get; set; }
+    public int SerialNumber { get; set; }
+    public string Tester { get; set; }
+    public int Bay { get; private set; }
+    public string MinServoPosition { get; private set; }
+    public string MaxServoPosition { get; private set; }
+    public string MinBuslinkPosition { get; private set; }
+    public string MaxBuslinkPosition { get; private set; }
+    public string ServoStroke { get; private set; }
+}
