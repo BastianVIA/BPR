1be46615d5c5f4955b1cfc577aaf8b50da9b78d4
Bimmerlynge <mathiashome@hotmail.com>
2023-12-05 20:18:58 +0100

    Removing filter options

--- Frontend/Components/TestResultTable.razor.cs
+++ Frontend/Components/TestResultTable.razor.cs
@@ -7,30 +7,4 @@ public class TestResultTableBase : ComponentBase
 {
     [Parameter] public List<TestResult> TestResults { get; set; }
     [Parameter] public EventCallback<TestResult> OnActuatorSelected { get; set; }
-    
-    protected string[] FilterOptions =
-    {
-        "Work Order Number",
-        "Serial Number",
-        "Tester",
-        "Bay",
-        "Error Count"
-    };
-    private List<string> _currentFilters = new();
-
-    protected void UpdateFilters(List<string>? newFilters)
-    {
-        newFilters ??= new List<string>();
-        _currentFilters = newFilters;
-    }
-    
-    protected bool ShouldShowColumn(string name)
-    {
-        return _currentFilters.Contains(name);
-    }
-    
-    protected void SelectRow(TestResult testResult)
-    {
-        OnActuatorSelected.InvokeAsync(testResult);
-    }
 }
