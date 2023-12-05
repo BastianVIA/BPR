83622e2fe28287148168af694987ff5456d0fc97
Bimmerlynge <mathiashome@hotmail.com>
2023-12-05 20:24:47 +0100

    chore: Code behind for AlertBox

---
+++ Frontend/Components/AlertBox.razor.cs
@@ -0,0 +1,43 @@
+﻿using Frontend.Service.AlertService;
+using Microsoft.AspNetCore.Components;
+
+namespace Frontend.Components;
+
+public class AlertBoxBase : ComponentBase, IDisposable
+{
+    protected Alert? Alert { get; set; } = null;
+    private int _alertTimeSeconds = 5;
+    private CancellationTokenSource? _cancelAlert;
+
+    protected override void OnInitialized()
+    {
+        AlertService.OnAlertEvent += HandleAlert;
+    }
+    
+    public void Dispose()
+    {
+        AlertService.OnAlertEvent -= HandleAlert;
+    }
+
+    private void HandleAlert(Alert alert)
+    {
+        Alert = alert;
+        InvokeAsync(StateHasChanged);
+        HideAlertTimer();
+    }
+    
+    private void HideAlertTimer()
+    {
+        _cancelAlert?.Cancel();
+        _cancelAlert = new CancellationTokenSource();
+        var cancelToken = _cancelAlert.Token;
+        Task.Delay(_alertTimeSeconds * 1000, cancelToken).ContinueWith(_ =>
+        {
+            if (!cancelToken.IsCancellationRequested)
+            {
+                Alert = null;
+                InvokeAsync(StateHasChanged);
+            }
+        });
+    }
+}
