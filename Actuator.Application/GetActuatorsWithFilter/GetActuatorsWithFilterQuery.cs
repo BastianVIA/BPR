d7f713e6aa5266829a2498a5da1eb4f5bed23571
Solaiman1991 <satar03@msn.com>
2023-12-05 10:00:58 +0100

    fix(7): Resolve comments

--- Actuator.Application/GetActuatorsWithFilter/GetActuatorsWithFilterQuery.cs
+++ Actuator.Application/GetActuatorsWithFilter/GetActuatorsWithFilterQuery.cs
@@ -10,16 +10,15 @@ public class GetActuatorsWithFilterQuery : IQuery<GetActuatorsWithFilterDto>
     internal string? ItemNumber { get; }
     internal int? ManufacturerNumber { get; }
     internal int? ProductionDateCode { get; }
-    
     internal string? CommunicationProtocol { get; }
-    
     internal string? ArticleNumber { get; }
-    
     internal string? ArticleName { get; }
     internal DateTime? StartDate { get; }
     internal DateTime? EndDate { get; }
 
-    private GetActuatorsWithFilterQuery(int? woNo, int? serialNo, string? pcbaUid, string? itemNo, int? manufacturerNo, int? productionDateCode, string? communicationProtocol, string? articleNumber, string? articleName, DateTime? startDate, DateTime? endDate)
+    private GetActuatorsWithFilterQuery(int? woNo, int? serialNo, string? pcbaUid, string? itemNo, int? manufacturerNo,
+        int? productionDateCode, string? communicationProtocol, string? articleNumber, string? articleName,
+        DateTime? startDate, DateTime? endDate)
     {
         WorkOrderNumber = woNo;
         SerialNumber = serialNo;
@@ -34,15 +33,19 @@ public class GetActuatorsWithFilterQuery : IQuery<GetActuatorsWithFilterDto>
         EndDate = endDate;
     }
 
-    public static GetActuatorsWithFilterQuery Create(int? woNo, int? serialNo, string? pcbaUid, string? itemNo, int? manufacturerNo, int? productionDateCode,string? communicationProtocol, string? articleNumber, string? articleName, DateTime? startDate, DateTime? endDate)
+    public static GetActuatorsWithFilterQuery Create(int? woNo, int? serialNo, string? pcbaUid, string? itemNo,
+        int? manufacturerNo, int? productionDateCode, string? communicationProtocol, string? articleNumber,
+        string? articleName, DateTime? startDate, DateTime? endDate)
     {
         if (woNo == null && serialNo == null && productionDateCode == null && manufacturerNo == null &&
             productionDateCode == null && pcbaUid == null &&
-            itemNo == null && communicationProtocol == null && articleNumber == null && articleName == null && startDate == null && endDate == null)
+            itemNo == null && communicationProtocol == null && articleNumber == null && articleName == null &&
+            startDate == null && endDate == null)
         {
             throw new ArgumentException("Must specify at least one search parameter");
         }
 
-        return new GetActuatorsWithFilterQuery(woNo, serialNo, pcbaUid, itemNo, manufacturerNo, productionDateCode, communicationProtocol, articleNumber, articleName, startDate, endDate);
+        return new GetActuatorsWithFilterQuery(woNo, serialNo, pcbaUid, itemNo, manufacturerNo, productionDateCode,
+            communicationProtocol, articleNumber, articleName, startDate, endDate);
     }
 }
