a88e4bc168a973ffb1add45e026c1051e3028eae
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-12-05 08:38:47 +0100

    feat(12): adding endpoint to retrieve data

---
+++ Actuator.Application/GetPCBAChangesForActuator/GetPCBAChangesForActuatorDto.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Entities;
+
+namespace Application.GetPCBAChangesForActuator;
+
+public class GetPCBAChangesForActuatorDto
+{
+    public List<GetPCBAChangesForActuatorChangeDto> Changes { get; }
+
+    private GetPCBAChangesForActuatorDto()
+    {
+    }
+
+    public GetPCBAChangesForActuatorDto(List<GetPCBAChangesForActuatorChangeDto> changes)
+    {
+        Changes = changes;
+    }
+
+    internal static GetPCBAChangesForActuatorDto From(List<ActuatorPCBAChange> pcbaChanges)
+    {
+        List<GetPCBAChangesForActuatorChangeDto> changes = new();
+        foreach (var change in pcbaChanges)
+        {
+            changes.Add(GetPCBAChangesForActuatorChangeDto.From(change));
+        }
+
+        return new GetPCBAChangesForActuatorDto(changes);
+    }
+}
+
+public class GetPCBAChangesForActuatorChangeDto
+{
+    public int WorkOrderNumber { get; }
+    public int SerialNumber { get; }
+    public string OldPCBAUid { get; }
+    public DateTime RemovalTime { get; }
+
+    private GetPCBAChangesForActuatorChangeDto()
+    {
+    }
+
+    private GetPCBAChangesForActuatorChangeDto(int woNo, int serialNo, string oldPCBAUid, DateTime removalTime)
+    {
+        OldPCBAUid = oldPCBAUid;
+        WorkOrderNumber = woNo;
+        SerialNumber = serialNo;
+        RemovalTime = removalTime;
+    }
+
+    internal static GetPCBAChangesForActuatorChangeDto From(ActuatorPCBAChange change)
+    {
+        return new GetPCBAChangesForActuatorChangeDto(change.WorkOrderNumber, change.SerialNumber, change.OldPCBAUid,
+            change.RemovalTime);
+    }
+}
