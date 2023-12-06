35dc21a0c3c4599b2c081413df17f2db316da18d
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-12-06 23:00:10 +0100

    feat(18): Force some required for filter to work

--- TestResult.Application/GetTestErrorsWithFilter/GetTestErrorsWithFilterQuery.cs
+++ TestResult.Application/GetTestErrorsWithFilter/GetTestErrorsWithFilterQuery.cs
@@ -1,16 +1,21 @@
-﻿using BuildingBlocks.Application;
+﻿using System.ComponentModel.DataAnnotations;
+using BuildingBlocks.Application;
 
 namespace TestResult.Application.GetTestErrorsWithFilter;
 
 public class GetTestErrorsWithFilterQuery : IQuery<GetTestErrorsWithFilterDto>
 {
+    [Required]
     public int TimeIntervalBetweenRowsAsMinutes { get; set; }
+    [Required]
+    public DateTime StartDate { get; set; }
+    [Required]
+    public DateTime EndDate { get; set; }
     public int? WorkOrderNumber { get; set; }
     public string? Tester { get; set; }
     public int? Bay { get; set; }
     public int? ErrorCode { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
+
     
     public GetTestErrorsWithFilterQuery()
     {}
