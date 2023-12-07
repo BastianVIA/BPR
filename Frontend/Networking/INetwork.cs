182774cf7bd4b9a2371fa8c0baabc9e46ab2c27c
Bimmerlynge <mathiashome@hotmail.com>
2023-12-07 23:30:28 +0100

    Re-adding lost Start-Enddate?

--- Frontend/Networking/INetwork.cs
+++ Frontend/Networking/INetwork.cs
@@ -10,7 +10,7 @@ public interface INetwork
      Task<GetActuatorWithFilterResponse> GetActuatorWithFilter(int? woNo, int? serialNo, string? pcbaUid, string? itemNo, int? manufacturerNo,
          int? productionDateCode, DateTime? createdTimeStart, DateTime? createdTimeEnd,string? software, string? configNo, string? articleName, string? articleNo, string? comProtocol);
      Task<GetTestResultsWithFilterResponse> GetTestResultWithFilter(int? woNo, int? serialNo,
-         string? tester, int? bay);
+         string? tester, int? bay,DateTime? startDate, DateTime? endDate);
      Task<byte[]> GetActuatorWithFilterAsCsv(List<CsvProperties> columnsToInclude, int? woNo, int? serialNo, string? pcbaUid, string? itemNo, int? manufacturerNo,
          int? productionDateCode, DateTime? createdTimeStart, DateTime? createdTimeEnd,string? software, string? configNo, string? articleName, string? articleNo, string? comProtocol);
 }
