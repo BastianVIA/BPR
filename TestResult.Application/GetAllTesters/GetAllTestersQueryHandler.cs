3a7546b7b72b09889fd88d5b08b5227ad5302644
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-12-07 15:27:18 +0100

    feat(19): endpoint to get all testers

---
+++ TestResult.Application/GetAllTesters/GetAllTestersQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.Application;
+using TestResult.Domain.Repositories;
+
+namespace TestResult.Application.GetAllTesters;
+
+public class GetAllTestersQueryHandler : IQueryHandler<GetAllTestersQuery,GetAllTestersDto>
+{
+    private readonly ITestResultRepository _testResultRepository;
+
+    public GetAllTestersQueryHandler( ITestResultRepository testResultRepository)
+    {
+        _testResultRepository = testResultRepository;
+    }
+
+    public async Task<GetAllTestersDto> Handle(GetAllTestersQuery request, CancellationToken cancellationToken)
+    {
+        var allTesters = await _testResultRepository.GetAllTesters();
+        return GetAllTestersDto.From(allTesters);
+    }
+}
--- TestResult.Domain/Repositories/ITestResultRepository.cs
+++ TestResult.Domain/Repositories/ITestResultRepository.cs
@@ -7,4 +7,5 @@ public interface ITestResultRepository
     Task CreateTestResult(Entities.TestResult testResult);
     Task<List<Domain.Entities.TestResult>> GetActuatorsTestDetails(int? woNo, int? serialNo, string? tester, int? bay);
     Task<int> GetNumberOfTestsPerformedInInterval(DateTime startTime, DateTime endTime);
+    Task<List<string>> GetAllTesters();
 }
