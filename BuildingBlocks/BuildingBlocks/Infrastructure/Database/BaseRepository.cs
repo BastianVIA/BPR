31aa65b1c84fe3c6ef655c44b7f9401c18117321
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-12-08 21:33:34 +0100

    fix(0): Remove unused

--- BuildingBlocks/BuildingBlocks/Infrastructure/Database/BaseRepository.cs
+++ BuildingBlocks/BuildingBlocks/Infrastructure/Database/BaseRepository.cs
@@ -19,11 +19,6 @@ public class BaseRepository<TEntity> where TEntity : class
     {
         return _dbContext.Set<TEntity>().AsQueryable();
     }
-    protected IQueryable<TEntity> QueryLocal()
-    {
-        return _dbContext.Set<TEntity>().Local.AsQueryable();
-    }
-
     protected IQueryable<T> QueryOtherLocal<T>() where T : class
     {
         return _dbContext.Set<T>().Local.AsQueryable();
