b812a8ac86849b5ab304d2af825f4ff800c91e7f
Bimmerlynge <mathiashome@hotmail.com>
2023-12-13 01:11:02 +0100

    Remove redunant code in config

--- LINTest/Config.cs
+++ LINTest/Config.cs
@@ -7,26 +7,17 @@ public static class Config
 {
     public static IServiceCollection AddLINTestServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var fileProcessorOptions = new FileProcessorOptions();
-        configuration.GetSection("FileProcessor").Bind(fileProcessorOptions);
-
-        services.AddSingleton(fileProcessorOptions);
-        var stateManagerOptions = new StateManagerOptions();
-        configuration.GetSection("StateManager").Bind(stateManagerOptions);
-
-
         services.AddSingleton<ConfigurationManager>();
-        services.AddSingleton(stateManagerOptions);
         services.AddHostedService<LINTestBackgroundService>();
         services.AddSingleton<FileProcessor>();
         services.AddSingleton<CsvDataService>();
         services.AddSingleton<FileProcessingStateManager>();
         
-        services.AddSingleton<FileProcessorOptions>(serviceProvider => 
-            configuration.GetSection("LINTest:FileProcessor").Get<FileProcessorOptions>());
+        services.AddSingleton<FileProcessorOptions>(_ => 
+            configuration.GetSection("LINTest:FileProcessor").Get<FileProcessorOptions>()!);
 
-        services.AddSingleton<StateManagerOptions>(serviceProvider => 
-            configuration.GetSection("LINTest:StateManager").Get<StateManagerOptions>());
+        services.AddSingleton<StateManagerOptions>(_ => 
+            configuration.GetSection("LINTest:StateManager").Get<StateManagerOptions>()!);
 
         return services;
     }
