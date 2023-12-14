0bfeb66426be7578a5c562f1642ce06aade99ba8
Bimmerlynge <mathiashome@hotmail.com>
2023-12-14 10:21:05 +0100

    rename parameter for test object creator method

--- Actuator.Tests/Util/EntityCreator.cs
+++ Actuator.Tests/Util/EntityCreator.cs
@@ -6,7 +6,7 @@ namespace Backend.Tests.Util;
 public static class EntityCreator
 {
     private static Fixture _fixture = new();
-    public static Domain.Entities.Actuator CreateActuator(int? woNo = null, int? serialNo = null, string? pcbaUid = null, int? manuNumber = null, string? itemNumber = null, string? software = null, int? prodDateCode = null, string? configNo = null, string? artNo = null, string? artName = null, string? commProtocol = null, DateTime? createdTime = null)
+    public static Domain.Entities.Actuator CreateActuator(int? woNo = null, int? serialNo = null, string? pcbaUid = null, int? manufacturerNo = null, string? itemNumber = null, string? software = null, int? prodDateCode = null, string? configNo = null, string? artNo = null, string? artName = null, string? commProtocol = null, DateTime? createdTime = null)
     {
         return Domain.Entities.Actuator.Create(
             CompositeActuatorId.From(
@@ -14,7 +14,7 @@ public static class EntityCreator
                 serialNo ?? _fixture.Create<int>()),
             PCBA.Create(
                 pcbaUid ?? _fixture.Create<string>(),
-                manuNumber ?? _fixture.Create<int>(),
+                manufacturerNo ?? _fixture.Create<int>(),
                 itemNumber ?? _fixture.Create<string>(),
                 software ?? _fixture.Create<string>(),
                 prodDateCode ?? _fixture.Create<int>(),
