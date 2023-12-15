dfb5be328a691e0675a7bf9cadd5fa3acbe74378
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-12-15 08:02:21 +0100

    fix(0): Controller tests til TestResult slice
    Co-authored-by: BastianVIA <70633697+bastianvia@users.noreply.github.com>
    Co-authored-by: Solaiman1991 <satar03@msn.com>
    Co-authored-by: Bimmerlynge <mathiashome@hotmail.com>

---
+++ Backend.Tests/Controllers/TestResult/GetAllTestersControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using AutoFixture;
+using Backend.Controllers.TestResult;
+using BuildingBlocks.Application;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using TestResult.Application.GetAllTesters;
+
+namespace Backend.Tests.Controllers.TestResult;
+
+public class GetAllTestersControllerTests
+{
+    private Fixture _fixture = new();
+    private GetAllTestersController _controller;
+    private readonly IQueryBus _bus = Substitute.For<IQueryBus>();
+
+    public GetAllTestersControllerTests()
+    {
+        _controller = new GetAllTestersController(_bus);
+    }
+
+    [Fact]
+    public async Task GetAsync_ReturnsOk_WhenNoTesterExists()
+    {
+        //Arrange
+        var dto = GetAllTestersDto.From(new List<string>());
+
+        _bus.Send(Arg.Any<GetAllTestersQuery>(), CancellationToken.None).Returns(dto);
+        
+        //Act
+        var result = await _controller.GetAsync(CancellationToken.None);
+        
+        //Assert
+        Assert.IsType<OkObjectResult>(result);
+    }
+    
+    [Fact]
+    public async Task GetAsync_ReturnsOk_WhenOneTesterExists()
+    {
+        //Arrange
+        var dto = GetAllTestersDto.From(new List<string>(){"Tester1"});
+
+        _bus.Send(Arg.Any<GetAllTestersQuery>(), CancellationToken.None).Returns(dto);
+        
+        //Act
+        var result = await _controller.GetAsync(CancellationToken.None);
+        
+        //Assert
+        Assert.IsType<OkObjectResult>(result);
+    }
+    
+    [Fact]
+    public async Task GetAsync_ReturnsOk_WhenManyTesterExists()
+    {
+        //Arrange
+        var dto = GetAllTestersDto.From(new List<string>(){"Tester1", "Tester2","Tester3"});
+
+        _bus.Send(Arg.Any<GetAllTestersQuery>(), CancellationToken.None).Returns(dto);
+        
+        //Act
+        var result = await _controller.GetAsync(CancellationToken.None);
+        
+        //Assert
+        Assert.IsType<OkObjectResult>(result);
+    }
+}
---
+++ Backend.Tests/Controllers/TestResult/GetTestResultsWithFilterControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoFixture;
+using Backend.Controllers.TestResult;
+using BuildingBlocks.Application;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using TestResult.Application.GetTestResultsWithFilter;
+
+namespace Backend.Tests.Controllers.TestResult;
+
+public class GetTestResultsWithFilterControllerTests
+{
+    private Fixture _fixture = new();
+    private GetTestResultsWithFilterController _controller;
+    private readonly IQueryBus _bus = Substitute.For<IQueryBus>();
+
+    public GetTestResultsWithFilterControllerTests()
+    {
+        _controller = new GetTestResultsWithFilterController(_bus);
+    }
+    
+    [Fact]
+    public async Task GetAsync_ReturnsOk_WhenNoTesterExists()
+    {
+        //Arrange
+        List<global::TestResult.Domain.Entities.TestResult> emptyResults = new();
+
+        var request = _fixture.Create<GetTestResultsWithFilterQuery>();
+        var dto = GetTestResultsWithFilterDto.From(emptyResults);
+
+        _bus.Send(Arg.Any<GetTestResultsWithFilterQuery>(), CancellationToken.None).Returns(dto);
+        
+        //Act
+        var result = await _controller.GetAsync(request, CancellationToken.None);
+        
+        //Assert
+        Assert.IsType<OkObjectResult>(result);
+    }
+
+}
